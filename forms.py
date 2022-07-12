35be1ebb88026d1c22b63d5e173a29055709f4ea
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2022-07-12 16:14:55 -0400

    added a render_kw field to each field so the html will have a placeholder

--- forms.py
+++ forms.py
@@ -15,26 +15,39 @@ class RegistrationForm(FlaskForm):
 
     """
     username = StringField(label='username', validators=[DataRequired(),
-                                                         Length(min=3, max=20)])
-    email = StringField(label='email', validators=[DataRequired(), Email()])
+                                                         Length(min=3, max=20)], render_kw={'placeholder': 'username'})
+    email = StringField(label='email', validators=[DataRequired(), Email()], render_kw={'placeholder': 'email'})
 
     password = PasswordField(label='password', validators=[DataRequired(),
-                                                           Length(min=6, max=12)])
+                                                           Length(min=6, max=12)],
+                             render_kw={'placeholder': 'password'})
 
     confirm_password = PasswordField(label='confirm password', validators=[DataRequired(),
                                                                            Length(min=6, max=12),
-                                                                           EqualTo('password')])
+                                                                           EqualTo('password')],
+                                     render_kw={'placeholder': 'confirm password'})
     submit = SubmitField(label='register')
+
+
+def validate_email_or_username(form, field):
+    # TODO Check if passed in field is a username or email in the database
+    return True
 
 
 class LoginForm(FlaskForm):
     """Form that logs users into the application.
 
     """
-    username = StringField(label='username', validators=[DataRequired(),
-                                                         Length(min=3, max=20)])
+    email_or_username = StringField(label='email_or_username', validators=[DataRequired(),
+                                                                           Length(min=3, max=20),
+                                                                           validate_email_or_username],
+                                    render_kw={'placeholder': 'Email address or username'})
+
     password = PasswordField(label='password', validators=[DataRequired(),
-                                                           Length(min=6, max=12)])
+                                                           Length(min=6, max=12)],
+                             render_kw={'placeholder': 'password'})
     remember = BooleanField(label='remember me')
 
     submit = SubmitField(label='login')
+
+
