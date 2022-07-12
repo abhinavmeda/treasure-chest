0b504f4046f99d279b863e8c0d628229d8c33f5e
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2022-07-12 10:43:19 -0400

    modified homepage route and imported decouple to hide key for form csrf token

--- routes.py
+++ routes.py
@@ -1,47 +1,47 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, flash, url_for
 from forms import RegistrationForm, LoginForm, RedirectToLoginOrRegister
-import requests
-import requests.auth
-import string
-import random
-
+from decouple import config
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ab6f63cb8ebded43ea3cb70bcb9389fa'
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     button = RedirectToLoginOrRegister()
-    if request.method == 'GET':
-        return render_template("homepage.html", form=button)
-    if request.method == 'POST':
+    if button.is_submitted():
         if 'login' in request.form:
             return redirect("/login")
         elif 'register' in request.form:
             return redirect("/register")
+    else:
+        return render_template("homepage.html", form=button)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     return render_template('login.html', form=form)
-    # if request.method == "GET":
-    #     return render_template("login.html")
-    # else:
-    #     # TODO check if form fields for username and email exist in database.
-    #     return "Account logged in as {}".format(request.form.get("value"))
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'GET':
-        form = RegistrationForm()
-        return render_template('register.html', form=form)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash("Account created for {}".format(form.username.data), 'success')
+        return redirect(url_for('homepage'))
     else:
-        s = ""
-        for k in request.form:
-            s += "{} = {} ".format(k, request.form[k])
-        return s
+        pass
+        # TODO flash("Account created for {}".format(form.username.data), 'danger')
+    return render_template('register.html', form=form)
+    # if request.method == 'GET':
+    #     return render_template('register.html', form=form)
+    # else:
+    #     if form.validate_on_submit():
+    #         s = ""
+    #         for k in request.form:
+    #             s += "{} = {} ".format(k, request.form[k])
+    #         return s
+    #     return "no"
     # if request.method == "GET":
     #     return render_template("register.html")
     # else:
